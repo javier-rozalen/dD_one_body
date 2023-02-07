ad8483ebbd05e776e2c4157260ec7a30d6903895
javier-rozalen <javiroza11@gmail.com>
2023-02-07 13:31:15 +0100

    sync

--- modules/loss_functions.py
+++ modules/loss_functions.py
@@ -13,19 +13,19 @@ def HO_energy(model, train_data, w_i, x2_y2_z2, target_dD):
                                  grad_outputs=torch.ones_like(psi),
                                  retain_graph=True)
     d_psi2 = d_psi **2
-    N = torch.dot(psi2, w_i) # squared ANN norm
+    N = w_i * torch.sum(psi2) # squared ANN norm
     psi_normalized = psi / torch.sqrt(N)
     
     
     # ANN energy
     K_vec = torch.sum(d_psi2, dim=1) # Local K
     U_vec = (psi2) * x2_y2_z2 # Local U
-    K = 0.5 * torch.dot(K_vec, w_i) / N # Integrated K
-    U = 0.5 * torch.dot(U_vec, w_i) / N # Integrated U
+    K = 0.5 * w_i * torch.sum(K_vec) / N # Integrated K
+    U = 0.5 * w_i * torch.sum(U_vec) / N # Integrated U
     E = K + U 
     
     # Overlap
-    overlap = torch.dot(psi_normalized * target_dD, w_i)
+    overlap = w_i * torch.sum(psi_normalized * target_dD)
     #print(psi, d_psi, d_psi2, E)
     
     return E, K, U, psi_normalized, overlap**2
